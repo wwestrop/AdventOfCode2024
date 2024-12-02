7a4c90428bfa5631a6e89427f1b446d6fb5604fc
Will Westrop <wwestrop@hotmail.com>
2024-12-02 19:40:10 +0000

    Day 1 - part 2

--- day_01.py
+++ day_01.py
@@ -1,7 +1,7 @@
 from util import get_input, get_int_pairs, run_for
 
 
-def day_01(filename: str):
+def pairwise_sum_difference(filename: str):
     lines = list(get_input(filename, get_int_pairs))
 
     list1 = sorted([v[0] for v in lines])
@@ -14,5 +14,38 @@ def day_01(filename: str):
     return diff
 
 
-run_for("inputs/day_01/example.txt", day_01)
-run_for("inputs/day_01/part1.txt", day_01)
+def calculate_incidence_map(list: list[int]):
+    result: dict[int, int] = {}
+
+    for item in list:
+        if item in result:
+            result[item] = result[item] + 1
+        else:
+            result[item] = 1
+
+    return result
+
+
+def similarity_score(filename: str):
+    lines = list(get_input(filename, get_int_pairs))
+
+    list1 = [v[0] for v in lines]
+    list2 = [v[1] for v in lines]
+
+    incidence_map = calculate_incidence_map(list2)
+
+    similarity = 0
+    for item in list1:
+        incidences_in_list2 = incidence_map[item] if item in incidence_map else 0
+        similarity += item * incidences_in_list2
+
+    return similarity
+
+
+run_for("inputs/day_01/example.txt", pairwise_sum_difference)
+run_for("inputs/day_01/input.txt", pairwise_sum_difference)
+
+print("-" * 50)
+
+run_for("inputs/day_01/example.txt", similarity_score)
+run_for("inputs/day_01/input.txt", similarity_score)
