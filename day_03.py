bb4c522d1afe607050ca1b666406c272518f4e5f
Will Westrop <wwestrop@hotmail.com>
2024-12-03 19:25:03 +0000

    Day 3 - part 2

--- day_03.py
+++ day_03.py
@@ -3,10 +3,8 @@ import re
 from util import get_input, run_for
 
 
-_FUNCTION_FINDER = re.compile("mul\\((?P<op1>\\d{1,3}),(?P<op2>\\d{1,3})\\)")
-
-
 def evaluate_instructions(filename: str):
+    _FUNCTION_FINDER = re.compile("mul\\((?P<op1>\\d{1,3}),(?P<op2>\\d{1,3})\\)")
     lines = list(get_input(filename))
 
     result = 0
@@ -21,4 +19,28 @@ def evaluate_instructions(filename: str):
     return result
 
 
-run_for(3, evaluate_instructions)
+def evaluate_instructions_2(filename: str):
+    _FUNCTION_FINDER = re.compile("(?P<func>[a-z']+)\\((?P<operands>[0-9,]*)\\)")
+    lines = list(get_input(filename))
+
+    enabled = True
+    result = 0
+
+    for line in lines:
+        for func_call in _FUNCTION_FINDER.finditer(line):
+            func = func_call.group("func")
+            if func.endswith("do"):
+                enabled = True
+            elif func.endswith("don't"):
+                enabled = False
+            elif func.endswith("mul"):
+                if enabled:
+                    operands = [int(o) for o in func_call.group("operands").split(",")]
+
+                    if len(operands) == 2:
+                        result += operands[0] * operands[1]
+
+    return result
+
+
+run_for(3, evaluate_instructions, evaluate_instructions_2)
